4feacefbf7e93aa40de91298d5dc4ea6f4fbf97e
Max <maksymus423@gmail.com>
2022-08-03 13:51:40 +0300

    Final

---
+++ Task2/Models/BookPostDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Task2.Models
+{
+    public class BookPostDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Cover { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public List<Rating> ratings { get; set; }
+    }
+}
