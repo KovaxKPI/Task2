55f5b63ca98ca3d629895623d9501aaf439e34c8
Max Kovalenko <maksymus423@gmail.com>
2023-04-09 15:53:32 +0300

    Added BookService

--- Task2/Controllers/BookController.cs
+++ Task2/Controllers/BookController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Task2.Interfaces;
 using Task2.Models;
 
 namespace Task2.Controllers
@@ -8,111 +9,45 @@ namespace Task2.Controllers
     [Route("api/[controller]")]
     public class BookController : ControllerBase
     {
-        private ApiContext _context;
-        public BookController(ApiContext context)
+        private readonly IBookService _bookService;
+        public BookController(IBookService bookService)
         {
-            _context = context;
+            _bookService= bookService;
         }
         [HttpGet]
-        public IEnumerable<BookDto> GetBooks()
+        public IQueryable<BookDto> GetBooks()
         {
-            var books = from b in _context.Books.Include(x => x.Ratings)
-                        .Include(y => y.Reviews)
-                        select new BookDto()
-                        {
-                            Id = b.Id,
-                            Title = b.Title,
-                            Author = b.Author,
-                            Rating = b.Ratings == null || b.Ratings.Count == 0 ? 0 : b.Ratings.Average(x => x.Score),
-                            reviewsNumber = b.Reviews == null || b.Reviews.Count == 0 ? 0 : b.Reviews.Count()
-                        };
-            return books;
+            return _bookService.GetBooks();
         }
         [HttpGet("{order:alpha}")]
-        public IEnumerable<BookDto> GetBooksByOrder(string order)
+        public IQueryable<BookDto> GetBooksByOrder(string order)
         {
-            var books = from b in _context.Books.Include(x => x.Ratings)
-                        .Include(y => y.Reviews)
-                        select new BookDto()
-                        {
-                            Id = b.Id,
-                            Title = b.Title,
-                            Author = b.Author,
-                            Rating = b.Ratings == null || b.Ratings.Count == 0 ? 0 : b.Ratings.Average(x => x.Score),
-                            reviewsNumber = b.Reviews == null || b.Reviews.Count == 0 ? 0 : b.Reviews.Count()
-                        };
-            if (order == "title")
-                return books.OrderBy(x => x.Title);
-            return books.OrderBy(x => x.Author);
+            return _bookService.GetBooksByOrder(order);
         }
         [HttpGet("{id:int}")]
-        public IEnumerable<BookDetailDto> GetBookById(int id)
+        public BookDetailDto? GetBookById(int id)
         {
-            var book = _context.Books.Include(x => x.Ratings)
-                        .Include(y => y.Reviews).FirstOrDefault(x => x.Id == id);
-            if(book == null)
-                return Enumerable.Empty<BookDetailDto>();
-            var b = new List<BookDetailDto>();
-            b.Add(new BookDetailDto()
-            {
-                Id = book.Id,
-                Title = book.Title,
-                Author = book.Author,
-                Cover = book.Cover,
-                Content = book.Content,
-                Rating = book.Ratings == null || book.Ratings.Count == 0 ?  0 : book.Ratings.Average(x => x.Score),
-                Reviews = book.Reviews
-            });
-            return b;
+            return _bookService.GetBookById(id);
         }
         [HttpPost]
-        public async Task<PostResponse> AddBook(BookPostDto book)
+        public async Task AddBook(BookPostDto book)
         {
-            var b = new Book();
-            b.Id = _context.Books.Count() + 1;
-            b.Title = book.Title;
-            b.Author = book.Author;
-            b.Cover = book.Cover;
-            b.Content = book.Content;
-            b.Genre = book.Genre;
-            b.Reviews = new List<Review>();
-            b.Ratings = book.Ratings;
-            _context.Books.Add(b);
-            await _context.SaveChangesAsync();
-            return new PostResponse() { Id = b.Id};
+            await _bookService.AddBook(book);
         }
         [HttpDelete("delete/{id:int}")]
-        public async void DeleteBook(int id)
+        public async Task DeleteBook(int id)
         {
-            var book = _context.Books.FirstOrDefault(x => x.Id == id);
-            if(book != null)
-            {
-                _context.Books.Remove(book);
-                await _context.SaveChangesAsync();
-            }
+            await _bookService.DeleteBook(id);
         }
         [HttpPut("{id:int}/review")]
-        public async Task<PostResponse> AddReview(int id, ReviewDto review)
+        public async Task AddReview(int id, ReviewDto review)
         {
-            Review r = new Review();
-            r.Id = review.Id;
-            r.Message = review.Message;
-            r.Reviewer = review.Reviewer;
-            r.BookId = id;
-            _context.Reviews.Add(r);
-            await _context.SaveChangesAsync();
-            return new PostResponse() { Id = r.Id };
+            await _bookService.AddReview(id, review);
         }
         [HttpPut("{id:int}/rate")]
-        public async Task<PostResponse> AddRate(int id, RatingDto rating)
+        public async Task AddRate(int id, RatingDto rating)
         {
-            Rating r = new Rating();
-            r.Id = rating.Id;
-            r.Score = rating.Score;
-            r.BookId = id;
-            _context.Ratings.Add(r);
-            await _context.SaveChangesAsync();
-            return new PostResponse() { Id = r.Id };
+            await _bookService.AddRate(id, rating);
         }
     }
 }
---
+++ Task2/Interfaces/IBookService.cs
@@ -0,0 +1,15 @@
+﻿using Task2.Models;
+
+namespace Task2.Interfaces
+{
+    public interface IBookService
+    {
+        IQueryable<BookDto> GetBooks();
+        IQueryable<BookDto> GetBooksByOrder(string order);
+        BookDetailDto? GetBookById(int id);
+        Task AddBook(BookPostDto book);
+        Task DeleteBook(int id);
+        Task AddReview(int id, ReviewDto review);
+        Task AddRate(int id, RatingDto rating);
+    }
+}
